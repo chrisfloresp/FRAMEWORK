761619224b1d6312a42ad66085c2d85e71e20c20
chrisfloresp <chris.floresp@gmail.com>
2015-05-16 03:13:18 +0000

    primero

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  content    :string(255)
+#  blog_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
